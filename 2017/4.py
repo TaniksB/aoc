11e1a8e5efd9dc7c584b9d03cd1a05f35cf25271
TaniksB <krmalkama@gmail.com>
2026-04-07 18:57:48 +0200

    2017 4-2

--- 2017/4.py
+++ 2017/4.py
@@ -8,10 +8,14 @@ for phrase in phrase_list:
     word_list = phrase.split(" ")
     check_list = []
     for word in word_list:
-        if word in check_list:
+        sorted_word_list = sorted(word)
+        sorted_word = ""
+        for letter in sorted_word_list:
+            sorted_word += letter
+        if sorted_word in check_list:
             check_list.append("break")
         else:
-            check_list.append(word)
+            check_list.append(sorted_word)
     if "break" not in check_list:
         valid_num += 1
 
