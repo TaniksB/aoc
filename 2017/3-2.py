24ae67c5b68ef2a5fc122af25d02f976e94a5d45
TaniksB <krmalkama@gmail.com>
2026-04-14 19:55:31 +0200

    2017 3-2

---
+++ 2017/3-2.py
@@ -0,0 +1,85 @@
+puzzle_input = 312051
+
+import sys
+
+class square:
+    def __init__(self, num, x, y):
+        self.num = num
+        self.x = x
+        self.y = y
+        self.man_dist = 0
+
+    def calculate_man_dist(self):
+        if self.x >= 0:
+            x = self.x
+        else:
+            x = -self.x
+        if self.y >= 0:
+            y = self.y
+        else:
+            y = -self.y
+        self.man_dist = x + y
+
+def get_counter(crd_x, crd_y, snail):
+    counter = 0
+    range = (-1, 0, 1)
+    for num in snail:
+        if num.x - crd_x in range:
+            if num.y - crd_y in range:
+                counter += num.num
+    return counter
+        
+
+counter = 1
+crd_x = 0
+crd_y = 0
+step_counter = 0
+steps = 1
+snail = []
+mode = 3 # 0 = up, 1 = left, 2 = down, 3 = right
+newmode = None
+while counter <= puzzle_input:
+        num = square(counter, crd_x, crd_y)
+        num.calculate_man_dist()
+        snail.append(num)
+        if mode == 0:
+            crd_y += 1
+        if mode == 1:
+            crd_x -= 1
+        if mode == 2:
+            crd_y -= 1
+        if mode == 3:
+            crd_x += 1
+        step_counter += 1
+        if step_counter == steps:
+            if mode % 2 == 0:
+                steps += 1
+            step_counter = 0
+            mode += 1
+        if mode == 4:
+            mode = 0
+        counter = get_counter(crd_x, crd_y, snail)
+        print (f'Number {num.num} with coordinates x{num.x} y{num.y} has a Manhattan Distance of {num.man_dist}!')
+num = square(counter, crd_x, crd_y)
+num.calculate_man_dist()
+snail.append(num)
+if mode == 0:
+    crd_y += 1
+if mode == 1:
+    crd_x -= 1
+if mode == 2:
+    crd_y -= 1
+if mode == 3:
+    crd_x += 1
+step_counter += 1
+if step_counter == steps:
+    if mode % 2 == 0:
+        steps += 1
+    step_counter = 0
+    mode += 1
+if mode == 4:
+    mode = 0
+counter = get_counter(crd_x, crd_y, snail)
+print (f'Number {num.num} with coordinates x{num.x} y{num.y} has a Manhattan Distance of {num.man_dist}!')
+sys.exit()
+
