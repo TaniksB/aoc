d18840fac14c57b384548118f4bd2e35344bd75a
TaniksB <krmalkama@gmail.com>
2026-06-09 22:47:37 +0200

    2018 3-2

--- 2018/3.js
+++ 2018/3.js
@@ -15,9 +15,10 @@ for (line of splitInput) {
     const dimensions = lineDetails[3].split("x");
     claim.width = Number(dimensions[0]);
     claim.height = Number(dimensions[1]);
+    claim.squares = new Set()
     claims.push(claim);
 }
-//claims = claims.slice(0,2)
+// claims = claims.slice(0,2)
 
 
 
@@ -25,16 +26,24 @@ let claimedSquares = [];
 let overlaps = new Set();
 //log claims on the fabric
 for (claim of claims) {
+    let unique = true
     const x_end = claim.xOffset + claim.width
     const y_end = claim.yOffset + claim.height
     for (let x = claim.xOffset; x < x_end; x++) {
         for (let y = claim.yOffset; y < y_end; y++) {
             let claimText = `${x}.${y}`
+            claim.squares.add(claimText)
             if (claimedSquares.includes(claimText)) {overlaps.add(claimText)}
             else {claimedSquares.push(claimText)}
         }
     }
     console.log(`Processed Claim #${claim.no}`)
+}
+
+
+for (claim of claims) {
+    const pleaseBeZero = overlaps.intersection(claim.squares)
+    if (pleaseBeZero.size == 0) {console.log(`Claim #${claim.no} does not overlap!`); break}
 }
 
 console.log(`${overlaps.size} Squares of fabric are overbooked!`)
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Accumulation of foolish [Advent of Code](https://adventofcode.com/) solutions
 ### 2017  -  Python  -  14/50
-### 2018  -  JavaScript  -  5/50
+### 2018  -  JavaScript  -  6/50
