ef98eef9cfb4e1c9370b920f61425a872dcc09f3
TaniksB <krmalkama@gmail.com>
2026-06-09 21:01:31 +0200

    2018 3-1

---
+++ 2018/3.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const input = fs.readFileSync('input_3.txt', 'utf8');
+
+const splitInput = input.split("\n");
+
+//interpret the claims
+let claims = []
+for (line of splitInput) {
+    const claim = {};
+    const lineDetails = line.split(" ");
+    claim.no = lineDetails[0].slice(1);
+    const offsets = lineDetails[2].split(",");
+    claim.xOffset = Number(offsets[0]);
+    claim.yOffset = Number(offsets[1].slice(0,offsets[1].length-1));
+    const dimensions = lineDetails[3].split("x");
+    claim.width = Number(dimensions[0]);
+    claim.height = Number(dimensions[1]);
+    claims.push(claim);
+}
+//claims = claims.slice(0,2)
+
+
+
+let claimedSquares = [];
+let overlaps = new Set();
+//log claims on the fabric
+for (claim of claims) {
+    const x_end = claim.xOffset + claim.width
+    const y_end = claim.yOffset + claim.height
+    for (let x = claim.xOffset; x < x_end; x++) {
+        for (let y = claim.yOffset; y < y_end; y++) {
+            let claimText = `${x}.${y}`
+            if (claimedSquares.includes(claimText)) {overlaps.add(claimText)}
+            else {claimedSquares.push(claimText)}
+        }
+    }
+    console.log(`Processed Claim #${claim.no}`)
+}
+
+console.log(`${overlaps.size} Squares of fabric are overbooked!`)
